b1f79223885b68ecd7e14e9218fa19293d2d90f4
Prakash Patil <prakash.patil@questionpro.com>
2019-03-25 19:11:14 +0530

    First Commitsdsa

--- QPDatabase/src/com/questionpro/db/handler/InsertTablehandler.java
+++ QPDatabase/src/com/questionpro/db/handler/InsertTablehandler.java
@@ -7,69 +7,59 @@ import java.io.*;
 public class InsertTablehandler implements QueryHandler {
     @Override
     public boolean validate(String query) {
-
         query = query.replaceAll("\\n", " ");
         query = query.replaceAll("\\s", " ");
-
-        query.toUpperCase();
-
         String[] tokenArray = query.split(" ");
         if (tokenArray != null && null != tokenArray[0] && "INSERT".equalsIgnoreCase(tokenArray[0]) &&
                 null != tokenArray[1] && "INTO".equalsIgnoreCase(tokenArray[1]) && null != tokenArray[2]) {
             if (checkIfTableExists(tokenArray[2])) {
-                //Table Exists
-
                 if (null != tokenArray[3] && tokenArray[3].toUpperCase().startsWith("VALUES(")) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        return true;
+    }
 
-
+    @Override
+    public void process(String query) {
+        query = query.replaceAll("\\n", " ");
+        query = query.replaceAll("\\s", " ");
+        String[] tokenArray = query.split(" ");
+        if (tokenArray != null && null != tokenArray[0] && "INSERT".equalsIgnoreCase(tokenArray[0]) &&
+                null != tokenArray[1] && "INTO".equalsIgnoreCase(tokenArray[1]) && null != tokenArray[2]) {
+            if (checkIfTableExists(tokenArray[2])) {
+                if (null != tokenArray[3] && tokenArray[3].toUpperCase().startsWith("VALUES(")) {
                     String[] values = query.toUpperCase().split("VALUES\\(");
-
                     if (null != values && null != values[1]) {
                         String[] parameters = values[1].split("\\);");
                         if (null != parameters[0] && !parameters[0].isEmpty()) {
                             String[] actualValuesare = parameters[0].split(",");
 
                             try {
-                                FileWriter fr = new FileWriter("/tmp/" + tokenArray[2] + "_data.txt",true);
+                                FileWriter fr = new FileWriter("/tmp/" + tokenArray[2] + "_data.txt", true);
                                 BufferedWriter br = new BufferedWriter(fr);
                                 PrintWriter pr = new PrintWriter(br);
                                 pr.println();
-                                for(int i =0;i<actualValuesare.length-1;i++){
+                                for (int i = 0; i < actualValuesare.length - 1; i++) {
                                     pr.print(actualValuesare[i]);
                                     pr.print("|");
                                 }
-                                pr.print(actualValuesare[actualValuesare.length-1]);
+                                pr.print(actualValuesare[actualValuesare.length - 1]);
 
                                 pr.close();
                                 br.close();
                                 fr.close();
 
                             } catch (Exception e) {
-                                return false;
+                                e.printStackTrace();
                             }
-
-
-
-                            return true;
                         }
                     }
                 }
             }
         }
-
-        return false;
-    }
-
-    @Override
-    public void process(String query) {
-        System.out.println("I am insert");
-        if (!validate(query)) {
-            System.out.println("Invalid Query for Insert");
-        } else {
-            System.out.println("Record Inserted Successfully");
-        }
-
-
     }
 
 
