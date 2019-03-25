6a4269688044ab2a9ae0dbcc4a6e2beefdec7acf
shwetadarekar <shweta.darekar@questionpro.com>
2019-03-25 16:33:06 +0530

    Adding Insert command

--- QPDatabase/src/com/questionpro/db/handler/InsertTablehandler.java
+++ QPDatabase/src/com/questionpro/db/handler/InsertTablehandler.java
@@ -4,16 +4,36 @@ public class InsertTablehandler implements QueryHandler{
     @Override
     public boolean validate(String query) {
 
+        query = query.replaceAll("\\n", " ");
         query = query.replaceAll("\\s", " ");
+
         query.toUpperCase();
 
-        String[] tokenArray =  query.split("");
-        if(tokenArray!=null && tokenArray[0]!=null && "INSERT".equalsIgnoreCase(tokenArray[0]) &&
-                tokenArray[1]!=null && "INTO".equalsIgnoreCase(tokenArray[1]) && tokenArray[3]!=null){
-            if(checkIfTableExists(tokenArray[3])){
+        String[] tokenArray =  query.split(" ");
+        System.out.println("Token Array After removing white spaces and split with spaces is");
+        for(String token:tokenArray){
+            System.out.println(token);
+        }
+
+
+        if(tokenArray!=null && null!=tokenArray[0] && "INSERT".equalsIgnoreCase(tokenArray[0]) &&
+                null!=tokenArray[1] && "INTO".equalsIgnoreCase(tokenArray[1]) && null!=tokenArray[2] && null!=tokenArray[3] ){
+            if(checkIfTableExists(tokenArray[2])){
                 //Table Exists
 
+                if(tokenArray[3].toUpperCase().startsWith("VALUES(")){
 
+                    System.out.println("3rd token value is---"+tokenArray[3]);
+                    String[] values = query.toUpperCase().split("VALUES\\(");
+                    System.out.println("query Starts with VALUES");
+                    if(null!=values && null!=values[1] ){
+                        String[] parameters = query.split("\\);");
+                        if(null!=parameters[0] && !parameters[0].isEmpty()){
+                            String[] actualValuesare = parameters[0].split(",");
+
+                        }
+                    }
+                }
             }
         }
 
@@ -32,13 +52,15 @@ public class InsertTablehandler implements QueryHandler{
 
 
     public boolean checkIfTableExists(String tableName){
-        File folder = new File("your/path");// Take path from Praveen
+        System.out.println("Inside check if table exists");
+        File folder = new File("/home/newuser/Documents/Shweta");// Take path from Praveen
         File[] listOfFiles = folder.listFiles();
-
+        System.out.println("Check file Name with tableName"+tableName+"_"+"data.txt");
         for (int i = 0; i < listOfFiles.length; i++) {
             if (listOfFiles[i].isFile()) {
                 System.out.println("File " + listOfFiles[i].getName());
-                if(listOfFiles[i].getName().toUpperCase().startsWith(tableName)){
+                if(listOfFiles[i].getName().toUpperCase().equalsIgnoreCase(tableName+"_"+"data.txt")){
+                    System.out.println("Check IF checkIfTableExists is passed");
                     return true;
                 }
             }
